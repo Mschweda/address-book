832930bdff8e097c7ce12e77181ba603062b9da6
Mschweda <tahoe27@gmail.com>
2019-05-20 20:59:49 -0500

    add book

--- address.js
+++ address.js
@@ -1,36 +1,82 @@
-alert('hello');
-const userArray =[];
-const eachUser = document.querySelector(".eachUser")
+// alert('hello');
+// 
+// 
+let userId = 0;
+(function(){
+	const userArray =[];
+	const allUsers = document.querySelector("#user");
+
+
 class user {
-	constructor(name, image){
-		this.name = name
-		this.image = image
+	constructor(firstName, lastName, image, phone, email, dob, age){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.image = image;
+		this.phone = phone;
+		this.email = email;
+		this.dob = dob;
+		this.age = age;
 	}
 }
-const newArr = [];
- function get(){
-	fetch('https://randomuser.me/api/')
+
+ 
+	fetch('https://randomuser.me/api/?results=50')
  	.then(response => response.json())
  	.then(data => {
 		 data.results.forEach(function(element){
-			 let name  = element.name.first;
+			 let firstName  = element.name.first;
+			 let lastName = element.name.last
 			 let image = element.picture.large;
-			 userArray.push(new user(name, image));
+			 let phone = element.phone;
+			 let email = element.email;
+			 let dob = element.dob.age;
+			 let age = element.dob.age
+			 userArray.push(new user(firstName, lastName, image, phone, email, dob, age));
+			 createDom(firstName, lastName, image, phone, email, dob, age);
 		 });
-	});
-		const createDom = (name, picture) => {
-			getElementById('name')name.innerHTML = `
-			user name: ${secondName}
-			<img src=${picture}
-			`
-		}
-		
-	
-		
-		
-		 
-	.catch( error => console.log("we have an error:", error))
- 	.finally( ()=> console.log("you suck"))
-  };
+		 console.log(userArray)
+	})
 
- }	
+	
+.catch( error  => console.log("Error!", error))
+.finally( ()=> console.log("this function always runs"))
+	  
+const createDom = (firstName, lastName, image, phone, email, dob, age) => {
+	allUsers.innerHTML += `
+	<div class='eachUser' id='eachuser'>
+	<div>
+		${firstName} ${lastName}
+		<br>
+		<img src='${image}'/>
+		</div>
+		<button onClick='displayInfo(${userId})'>More info</button>
+		<div id ='otherInfo${userId}' style='display:none;'>
+		Phone Number: ${phone}
+		<br>
+		Email: ${email}
+		<br>
+		Date Of Birth: ${dob}
+		<br>
+		Age: ${age}
+		</div>
+	</div>
+	`
+}
+}) ()
+
+
+
+
+
+// const rp = require('request-promise');
+
+// rp('https://jsonplaceholder.typicode.com/users/1')
+//     .then((body) => {
+//         const parsedData = JSON.parse(body);
+// 		console.log(`${parsedData.name} lives in
+// 		${parsedData.address.city}`);
+//     })
+//     .catch((err) => {
+// 		console.log('Error!', err);
+		
+//     });
